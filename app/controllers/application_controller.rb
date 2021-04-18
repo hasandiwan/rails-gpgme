f8e89e51cb59f0b4e653dee38655f13939a06881
Hasan Diwan <hd1@jsc.d8u.us>
2021-04-19 00:55:13 +0200

    activate verify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,16 @@ class ApplicationController < ActionController::API
     render json: {:text => params[:text], :signed => $out.to_s}, status: :created
   end
 
-  # TODO add verify, encryption, and descryption support here
+  def verify
+    $text = params[:text]
+    $key = params[:key]
+    $valid = false
+    $crypto = GPGME::Crypto.new
+    $crypto.verify($crypto.sign $text)  do |signature|
+      #render json: {:verified => signature.valid? }, status: 201
+      $vald = signature.valid?
+    end
+    render json: {:verified => $vald }, status: 201
+  end
+  # TODO add encryption, and descryption support here
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'sign', to: 'application#index'
+  post 'verify', to: 'application#verify'
 end
